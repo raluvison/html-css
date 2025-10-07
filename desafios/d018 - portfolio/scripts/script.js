651b5bbc2455df2276814302107bf3f3d4e6ccc2
Rafael Luvison <luvison.25@gmail.com>
2025-10-06 23:58:36 -0300

    projeto parcialmente finalizado

--- desafios/d018 - portfolio/scripts/script.js
+++ desafios/d018 - portfolio/scripts/script.js
@@ -1,26 +1,3 @@
-const botao = document.getElementById('botao-tema');
-const body = document.body;
-
-// Persistência do tema
-const temasalvo = localStorage.getItem('tema');
-temaEscuro(temasalvo === 'escuro');
-
-// Função para alternar entre tema claro e escuro
-function temaEscuro(tipo) {
-  if (tipo == true) {
-    body.classList.add('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-sun"></i>';
-  } else {
-    body.classList.remove('escuro');
-    botao.innerHTML = '<i class="fa-solid fa-moon"></i>';
-  }
-}
-
-botao.addEventListener('click', () => {
-  const isescuro = body.classList.toggle('escuro');
-  temaEscuro(isescuro);
-  localStorage.setItem('tema', isescuro ? 'escuro' : 'claro');
-});
 
 // Scroll suave para links de navegação
 const navLinks = document.querySelectorAll('#menu a.link');
